88c8436d93c01fe4094dad4105ffa11d2ae33b9c
Peter <pqk314@gmail.com>
2024-09-30 17:58:21 -0500

    added function to convert string to matrix

--- print_byte_mat.c
+++ print_byte_mat.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 void print_mat(u_int8_t mat[4][4])  {
     for (int i = 0; i < 4; i++) {
@@ -30,13 +31,28 @@ void fill_mat(u_int8_t mat[4][4]) {
         }
 }
 
+void string_to_mat(u_int8_t mat[4][4], char str[48]){
+    int counter = 0;
+    char *token = strtok(str, " ");
+    while(token) {
+        u_int8_t value = (int)strtol(token, NULL, 16);
+        mat[counter/4][counter%4] = value;
+        // printf("%02x (%d,%d)\n", value, counter/4, counter%4);
+        token = strtok(NULL, " ");
+        counter++;
+    }
+
+}
+
 
 
 int main(void) {
     srand(time(NULL));   // Initialization, should only be called once.
     u_int8_t mat[4][4];
+    char str[48] = "32 88 31 e0 43 5a 31 37 f6 30 98 07 a8 8d a2 34";
 
-    fill_mat(mat);
+    string_to_mat(mat, str);
+    // fill_mat(mat);
     print_mat(mat);
 
     return 0;
