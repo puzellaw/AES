5c60da9ed72d54f49b6524f433cc2403e933763c
Will Puzella <puzellaw@carleton.edu>
2024-09-30 11:24:41 -0500

    Commit of Unity testing framework to repo. Added a header file for all AES functions. Created scaffold for function testing framework.

---
+++ AES_functions.h
@@ -0,0 +1,74 @@
+/* 
+Header for Functions required to implement the AES cryptographic method.
+Adopted from NIST AES standard.
+
+Written by Will Puzella & Peter Kelly
+For Carleton College Computer Science Comps 2024
+*/
+
+
+
+/* The transformation of the state in which a round key is combined
+with the state. */
+void AddRoundKey();
+
+/* The block cipher specifed in this Standard with 128-bit keys. */
+void AES_128();
+
+/* The block cipher specifed in this Standard with 192-bit keys. */
+void AES_192();
+
+/* The block cipher specifed in this Standard with 256-bit keys. */
+void AES_256();
+
+/* The transformation of blocks that underlies AES-128, AES-192,
+and AES-256; the key schedule and the number of rounds are
+parameters of the transformation. */
+void Cipher();
+
+/* The inverse of CIPHER() in which dw replaces w as the key schedule parameter. */
+void EqinvCipher();
+
+/* The inverse of CIPHER(). */
+void InvCipher();
+
+/* The inverse of MIXCOLUMNS().*/
+void InvMixColumns();
+
+/* The inverse of SBOX(). */
+void InvSBox();
+
+/* The inverse of SHIFTROWS(). */
+void InvShiftRows();
+
+/* The inverse of SUBBYTES(). */
+void InvSubBytes();
+
+/* The routine that generates the round keys from the key. */
+void KeyExpansion();
+
+/* The routine that generates the modifed round keys for the equivalent
+inverse cipher. */
+void KeyExpansionEIC();
+/* The transformation of the state that takes all of the columns of the
+state and mixes their data (independently of one another) to produce
+new columns. */
+void MixColumns();
+/* The transformation of words in which the four bytes of the word
+are permuted cyclically. */
+void RotWord();
+/* The transformation of bytes defned by the S-box. */
+void SBox();
+/* The transformation of the state in which the last three rows are
+cyclically shifted by different offsets. */
+void ShiftRows();
+/* The transformation of the state that applies the S-box independently
+to each byte of the state. */
+void SubBytes();
+/* The transformation of words in which the S-box is applied to each
+of the four bytes of the word. */
+void SubWord();
+/* The transformation of bytes in which the polynomial representation
+of the input byte is multiplied by x, modulo m(x), to produce the
+polynomial representation of the output byte. */
+void XTimes();
