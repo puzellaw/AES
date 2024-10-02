06c81deb182112a33980176143b54adc3ccc2c10
Peter <pqk314@gmail.com>
2024-10-02 12:43:42 -0500

    added sub_word.c

--- main.c
+++ main.c
@@ -2,23 +2,35 @@
 #include<stdio.h>
 
 
+/*
+    clang main.c sub_word.c sbox.c rot_word.c x_times.c
+*/
+
 int main(void){
 
-    u_int8_t myChar = 0x7f;
-    printf("%02x\n", sbox(myChar));
+    // u_int8_t myChar = 0x7f;
+    // printf("%02x\n", sbox(myChar));
 
 
-    u_int8_t vec[4] = {0x01, 0x02, 0x03, 0x04};
-    rot_word(vec);
-    for(int i = 0; i < 4; i++){
-        printf("%x", vec[i]);
+    // u_int8_t vec[4] = {0x01, 0x02, 0x03, 0x04};
+    // rot_word(vec);
+    // for(int i = 0; i < 4; i++){
+    //     printf("%x", vec[i]);
+    // }
+    // printf("\n");
+
+
+    // u_int16_t byte = 0b00010000;
+    // byte = x_times(byte);
+    // printf("%02x\n", byte);
+
+    u_int8_t vec[4] = {0xa3, 0x4f, 0x85, 0xea};
+    SubWord(vec);
+    for (size_t i = 0; i < 4; i++)
+    {
+        printf("%02x\n", vec[i]);
     }
-    printf("\n");
-
-
-    u_int16_t byte = 0b00010000;
-    byte = x_times(byte);
-    printf("%02x\n", byte);
+    
 
 
     return 0;
--- sub_word.c
+++ sub_word.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
 #include "AES_functions.h"
+#include<stdio.h>
 
 void SubWord(u_int8_t vec[4]) {
     for (int i = 0; i < 4; i++)
