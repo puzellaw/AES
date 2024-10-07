8361ea76bab58a4c8b5f2b5a5447e144dd492367
Will Puzella <puzellaw@carleton.edu>
2024-10-07 12:10:59 -0500

    Updated mix Collumns

--- function_testing.c
+++ function_testing.c
@@ -113,7 +113,7 @@ void test_KeyExpansionEIC_correctOutput(void) {
 
 }
 
-void test_MixColumns_correctOutput(void) {
+void test_MixColumns_correctOutput(void) { // Not Passing
     u_int8_t input[4][4] = {{0xd4,0xe0,0xb8,0x1e},
                             {0xbf,0xb4,0x41,0x27},
                             {0x5d,0x52,0x11,0x98},
@@ -157,9 +157,9 @@ void test_SubWord_correctOutput(void) { //Done: Passing as of 10/2/24
     TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected, input, 4, "Output Should have been [0x7e, 0xcf,0x77, 0x16].\n");
 }
 
-void test_XTimes_correctOutput(void) { //Done: Passing as of 10/2/24
+void test_XTimes_correctOutput(void) { //Done: Passing as of 10/7/24
     u_int8_t input= 0x57;
-    u_int8_t expected = 0xfe;
+    u_int8_t expected = 0xae;
     TEST_ASSERT_EQUAL_UINT8_MESSAGE(expected, XTimes(input), "Expected output as defined in manuel is 0xfe");
 
 }
@@ -172,5 +172,6 @@ int main(void) {
     RUN_TEST(test_SubWord_correctOutput);
     RUN_TEST(test_AddRoundKey_correctOutput);
     RUN_TEST(test_MixColumns_correctOutput);
+    RUN_TEST(test_XTimes_correctOutput);
     return UNITY_END();
 }
--- mix_columns.c
+++ mix_columns.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include "AES_functions.h"
+//#include "x_times.c"
+
+u_int8_t Multiply(u_int8_t a, u_int8_t b) {
+    u_int8_t result = 0;
+    u_int8_t carry = 0;
+    for (int i = 0; i < 8; i++) {
+        if (b & 1) {
+            result = result ^ a;
+        }
+        b = b >> 1;
+        carry = a & 0b10000000;
+        a = a << 1; 
+        if (carry) {
+            a = a ^ 0x1b;
+        }
+    }
+    return result;
+}
 
 void MixColumns(u_int8_t buffer[4][4]) {
     u_int8_t tmp[4];
@@ -7,9 +25,14 @@ void MixColumns(u_int8_t buffer[4][4]) {
         for (int j = 0; j>4 ; j++) { 
             tmp[j] = buffer[i][j];
         }
-        buffer[i][0] = (XTimes(XTimes(tmp[0]))) ^ (XTimes(XTimes(XTimes(tmp[1])))) ^ tmp[2] ^ tmp[3];
-        buffer[i][1] = tmp[0] ^ (XTimes(XTimes(tmp[1]))) ^ (XTimes(XTimes(XTimes(tmp[2])))) ^ tmp[3];
-        buffer[i][2] = tmp[0] ^ tmp[1] ^ (XTimes(XTimes(tmp[2]))) ^ (XTimes(XTimes(XTimes(tmp[3]))));
-        buffer[i][3] = (XTimes(XTimes(XTimes(tmp[0])))) ^ tmp[1] ^ tmp[2] ^ (XTimes(XTimes(tmp[3])));
+        buffer[i][0] = (XTimes(tmp[0])) ^ (Multiply(tmp[1], 0x3)) ^ tmp[2] ^ tmp[3];
+        buffer[i][1] = tmp[0] ^ (XTimes(tmp[1])) ^ (Multiply(tmp[2], 0x3)) ^ tmp[3];
+        buffer[i][2] = tmp[0] ^ tmp[1] ^ (XTimes(tmp[2])) ^ (Multiply(tmp[3], 0x3));
+        buffer[i][3] = (Multiply(tmp[0], 0x3)) ^ tmp[1] ^ tmp[2] ^ (XTimes(tmp[3]));
     }
 }
+
+/* int main(void) {
+    printf("The result of math is %x\n", Multiply(0x53, 0xca));
+    return 1;
+} */
