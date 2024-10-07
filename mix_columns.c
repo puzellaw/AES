0ad68ede4c0de2d796ee7fe600a539b7a19310b8
Peter <pqk314@gmail.com>
2024-10-07 12:30:57 -0500

    working mix_columns

--- mix_columns.c
+++ mix_columns.c
@@ -21,18 +21,13 @@ u_int8_t Multiply(u_int8_t a, u_int8_t b) {
 
 void MixColumns(u_int8_t buffer[4][4]) {
     u_int8_t tmp[4];
-    for (int i = 0; i > 4; i++) {
-        for (int j = 0; j>4 ; j++) { 
-            tmp[j] = buffer[i][j];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j<4 ; j++) { 
+            tmp[j] = buffer[j][i];
         }
-        buffer[i][0] = (XTimes(tmp[0])) ^ (Multiply(tmp[1], 0x3)) ^ tmp[2] ^ tmp[3];
-        buffer[i][1] = tmp[0] ^ (XTimes(tmp[1])) ^ (Multiply(tmp[2], 0x3)) ^ tmp[3];
-        buffer[i][2] = tmp[0] ^ tmp[1] ^ (XTimes(tmp[2])) ^ (Multiply(tmp[3], 0x3));
-        buffer[i][3] = (Multiply(tmp[0], 0x3)) ^ tmp[1] ^ tmp[2] ^ (XTimes(tmp[3]));
+        buffer[0][i] = (XTimes(tmp[0])) ^ (Multiply(tmp[1], 0x3)) ^ tmp[2] ^ tmp[3];
+        buffer[1][i] = tmp[0] ^ (XTimes(tmp[1])) ^ (Multiply(tmp[2], 0x3)) ^ tmp[3];
+        buffer[2][i] = tmp[0] ^ tmp[1] ^ (XTimes(tmp[2])) ^ (Multiply(tmp[3], 0x3));
+        buffer[3][i] = (Multiply(tmp[0], 0x3)) ^ tmp[1] ^ tmp[2] ^ (XTimes(tmp[3]));
     }
 }
-
-/* int main(void) {
-    printf("The result of math is %x\n", Multiply(0x53, 0xca));
-    return 1;
-} */
