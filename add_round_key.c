686ce466c90debc183ef71b221e614a27fc35757
Peter <pqk314@gmail.com>
2024-10-09 11:21:38 -0500

    updated add_round_key.c to use u_int8_t

--- add_round_key.c
+++ add_round_key.c
@@ -1,14 +1,22 @@
 #include "AES_functions.h"
 #include<stdio.h>
 
-void AddRoundKey(u_int8_t state[4][4], u_int32_t roundKey[4]) {
-    u_int8_t partialRoundKey;
-    u_int8_t shiftedMask;
+// void AddRoundKey(u_int8_t state[4][4], u_int32_t roundKey[4]) {
+//     u_int8_t partialRoundKey;
+//     u_int8_t shiftedMask;
+//     for (int i = 0; i < 4; i++) {
+//         for (int j = 0; j < 4; j++) {
+//             partialRoundKey = roundKey[i];
+//             shiftedMask = (partialRoundKey >> (j * 8)) & 0xff;
+//             state[i][j] = state [i][j] ^ shiftedMask;
+//         }
+//     }
+// }
+
+void AddRoundKey(u_int8_t state[4][4], u_int8_t roundKey[4][4]) {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            partialRoundKey = roundKey[i];
-            shiftedMask = (partialRoundKey >> (j * 8)) & 0xff;
-            state[i][j] = state [i][j] ^ shiftedMask;
+            state[i][j] = state[i][j]^roundKey[i][j];
         }
     }
 }
