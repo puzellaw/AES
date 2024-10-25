9a84c12f54a819520fe15eba63341c918f155f6e
Will Puzella <puzellaw@carleton.edu>
2024-10-25 11:55:26 -0500

    CBC functionality and working decryption
    Co-authored-by: pqk314 <pqk314@users.noreply.github.com>

--- CBC.c
+++ CBC.c
@@ -124,24 +124,35 @@ u_int8_t *decryptFile(FILE *inputFile, FILE *outputFile, u_int8_t *key, u_int8_t
     char ch;
     int i = 0;
     u_int8_t previousVector[4][4];
+    u_int8_t initialVector[4][4];
     ch = fgetc(inputFile);
-    for (int q = 0; q < 15; q++) {
-        previousVector[q%4][q/4] = iv[q]; 
+    for (int q = 0; q < 16 ; q++) {
+        initialVector[q%4][q/4] = iv[q]; 
     }
     while (feof(inputFile) == 0) {
         printf("ch is %02x\n", (u_int8_t)ch);
         if (i == 15) {
             block[i%4][i/4] = (u_int8_t)ch;
+            for (int iter = 0; iter < 4; iter++)
+            {
+                for (int iter2 = 0; iter2 < 4; iter2++)
+                {
+                    previousVector[iter][iter2] = initialVector[iter][iter2];
+                    initialVector[iter][iter2] = block[iter][iter2];
+                }    
+            }
+            
             InvCipher(block, 10, keyExp);
             XOR(block,previousVector);
+            
          
             for (int j = 0; j < 16; j++) {
                 printf("%02x\n", block[j%4][j/4]);
-                previousVector[j%4][j/4] = block[j%4][j/4];
                 fputc(block[j%4][j/4], outputFile);
             }
             i = 0; 
-        } else {
+        }
+        else {
             block[i%4][i/4] = (u_int8_t)ch;
             i++;
         }
